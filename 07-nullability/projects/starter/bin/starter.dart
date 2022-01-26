bcbaa5307f25c2b6bd1515f708b160f81844f559
u7274374 <u7274374@anu.edu.au>
2022-01-26 21:06:17 +0800

    Done with challenge 7 and 8.

--- 07-nullability/projects/starter/bin/starter.dart
+++ 07-nullability/projects/starter/bin/starter.dart
@@ -1,3 +1,41 @@
+import 'dart:math';
+
+/// Write a function that randomly returns 42 or `null`. Assign the return value of
+/// the function to a variable named result that will never be `null`. Give `result`
+/// a default of 0 if the function returns `null`.
 void main() {
-  
+  // This is for challenge 1.
+  final int? result;
+  result = generateRandom() ?? 0;
+  print(result);
+
+ // This is for challenge 2.
+  var name = Name(givenName: 'Yojer', surname: 'Liu');
+  print(name);
+}
+
+int? generateRandom() {
+  var random = Random();
+  return (random.nextInt(2) == 0) ? 42 : null;
+}
+
+class Name {
+  String givenName;
+  String? surname;
+  bool surnameIsFirst;
+
+  Name({required this.givenName, this.surname, this.surnameIsFirst = false});
+
+  @override
+  String toString() {
+    if(surname == null) {
+      return '$givenName';
+    } else {
+      if(surnameIsFirst) {
+        return '$surname $givenName';
+      } else {
+        return '$givenName $surname';
+      }
+    }
+  }
 }
--- 08-collections/projects/starter/bin/starter.dart
+++ 08-collections/projects/starter/bin/starter.dart
@@ -1,3 +1,53 @@
 void main() {
+  const text = "I don't know what to say but resort to endless inner monologue. "
+      "Maybe that's why I'm accepted as an introverted person.";
 
+  // Challenge 1
+  final set = dealWithText(text);
+  print(set);
+
+  // Challenge 2
+  final codePointSet = text.runes.toSet();
+  final map = countCharacters(codePointSet, text)
+      .map((key, value) => MapEntry(String.fromCharCode(key), value));
+  print(map);
+
+  // Challenge 3
+  final userList = [User(1, 'Yojer'), User(2, 'Amy'), User(3, 'Bob')];
+  print(convertUserListToMap(userList));
+}
+
+/// write a function that takes a paragraph of text and returns a collection of unique
+/// `String` characters that the text contains.
+Set<String> dealWithText(String text) {
+  return text.runes.map((e) => String.fromCharCode(e)).toSet();
+}
+
+/// Repeat Challenge 1, but this time have the function return a collection that
+/// contains the frequency, or count, of every unique character.
+Map<int, int> countCharacters(Set<int> codePointSet, String text) {
+  final map = <int, int>{};
+  for (var element in codePointSet) {
+    map[element] = 0;
+  }
+  final codePointList = text.runes;
+  codePointList.forEach((element) => map[element] = map[element]! + 1);
+  return map;
+}
+
+/// Create a class called `User` with properties for `id` and `name`. Make a `List`
+/// with three users, specifying any appropriate names and IDs you like. Then write
+/// a function that converts your user list to a list of maps whose keys are
+/// `id` and `name`.
+Map<int, String> convertUserListToMap(List<User> userList) {
+  final userMap = <int, String>{};
+  userList.forEach((user) => userMap[user.id] = user.name);
+  return userMap;
+}
+
+class User {
+  final int id;
+  final String name;
+
+  User(this.id, this.name);
 }
